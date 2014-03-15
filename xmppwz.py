3c116af049b428e174db8fb32a9ead0436698006
Constantine Dolinin <kostett@siemenz.ru>
2014-03-15 17:00:26 +0300

    Update xmppwz.py
    [*] crash on zero message

--- xmppwz.py
+++ xmppwz.py
@@ -6,29 +6,30 @@ import xmpp, urllib2, ConfigParser
 
 def messageCB(conn,msg):
     command = msg.getBody()
-    command = command.strip()
-    check = re.compile(r'^[A-Za-z\s]*$')
-    if ( len(command) == 4 and check.match(command) ):
-        strURL = 'http://weather.noaa.gov/pub/data/observations/metar/decoded/' + command.upper() + '.TXT'
-        req = urllib2.Request(strURL)
-        try:
-            resp = urllib2.urlopen(req)
-        except urllib2.URLError, e:
-            if e.code == 404:
-                conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. Probably you input incorrect CODE or its weather not cached."))
+    if ( command is not None ):
+        command = command.strip()
+        check = re.compile(r'^[A-Za-z\s]*$')
+        if ( len(command) == 4 and check.match(command) ):
+            strURL = 'http://weather.noaa.gov/pub/data/observations/metar/decoded/' + command.upper() + '.TXT'
+            req = urllib2.Request(strURL)
+            try:
+                resp = urllib2.urlopen(req)
+            except urllib2.URLError, e:
+                if e.code == 404:
+                    conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. Probably you input incorrect CODE or its weather not cached."))
+                else:
+                    conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. METAR weather caching site seems down."))
             else:
-                conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. METAR weather caching site seems down."))
+                # 200, 301
+                if ( urllib2.urlopen(strURL).getcode() not in [200, 301] ):
+                    conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. METAR weather caching site seems down."))
+                else:
+                    f = urllib2.urlopen(urllib2.Request(strURL))
+                    response = f.read()
+                    f.close()
+                    conn.send(xmpp.Message(msg.getFrom(),response))
         else:
-            # 200, 301
-            if ( urllib2.urlopen(strURL).getcode() not in [200, 301] ):
-                conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. METAR weather caching site seems down."))
-            else:
-                f = urllib2.urlopen(urllib2.Request(strURL))
-                response = f.read()
-                f.close()
-                conn.send(xmpp.Message(msg.getFrom(),response))
-    else:
-        conn.send(xmpp.Message(msg.getFrom(),"Usage: CODE\nYou can find out code of your city airport here - http://www.rap.ucar.edu/weather/surface/stations.txt"))
+            conn.send(xmpp.Message(msg.getFrom(),"Usage: CODE\nYou  can find out code of your city airport here - http://www.rap.ucar.edu/weather/surface/stations.txt"))
 
 
 def presenceCB(conn,msg):
@@ -53,11 +54,12 @@ def main():
     password=(config.get('account', 'password'))
     presence=(config.get('presence','presence'))
     jid=xmpp.protocol.JID(user)
-    cl = xmpp.Client(jid.getDomain(), debug=[])
+    # cl = xmpp.Client(jid.getDomain()) # debug enabled
+    cl = xmpp.Client(jid.getDomain(), debug=[]) #debug disabled
     if cl.connect() == "":
         print "not connected"
         sys.exit(0)
-    if cl.auth(jid.getNode(),password,"METAR weather bot") == None:
+    if cl.auth(jid.getNode(),password,"METAR weather bot") is None:
         print "authentication failed"
         sys.exit(0)
     cl.UnregisterDisconnectHandler(cl.DisconnectHandler)
