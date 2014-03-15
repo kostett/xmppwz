f76b4dadb79e2ced1335b2ec069efd12d8fe387c
Constantine Dolinin <kostett@siemenz.ru>
2014-03-15 10:39:48 +0300

    Update xmppwz.py
    REWRITE IT ALL ARRGGH

--- xmppwz.py
+++ xmppwz.py
@@ -1,68 +1,70 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
 
-'''xmppwz - XMPP-bot which sends METAR weather data.
-licence GPL v.3, where it was stolen - don't remember, rewrite from ipbot'''
+import sys, os, signal, time, re
+import xmpp, urllib2, ConfigParser
 
-import xmpp
-import urllib2
-import ConfigParser
-import re
-
-config = ConfigParser.ConfigParser()
-config.read('/etc/xmppwz.conf')
-
-# work with cfg
-user=(config.get('account', 'login'))
-password=(config.get('account', 'password'))
-presence=(config.get('presence','presence'))
-
-jid=xmpp.protocol.JID(user)
-client=xmpp.Client(jid.getDomain(),debug=[])
-client.connect()
-client.auth(jid.getNode(),password,"METAR weather bot")
-
-# sending init presence
-def status(xstatus):
-    status=xmpp.Presence(status=xstatus,show=presence,priority='1')
-    client.send(msging)
-
-# subscribe all
-def presence(conn,mess):
-    if ( mess.getType() == "subscribe" ):
-        conn.send(xmpp.Presence(to=mess.getFrom(), typ='subscribed'))
-
-def message(conn,mess):
-    global client
-    command = mess.getBody() 
+def messageCB(conn,msg):
+    command = msg.getBody()
     command = command.strip()
     check = re.compile(r'^[A-Za-z\s]*$')
     if ( len(command) == 4 and check.match(command) ):
-        strURL='http://weather.noaa.gov/pub/data/observations/metar/decoded/' + command.upper() + '.TXT'
+        strURL = 'http://weather.noaa.gov/pub/data/observations/metar/decoded/' + command.upper() + '.TXT'
         req = urllib2.Request(strURL)
-        try:    
+        try:
             resp = urllib2.urlopen(req)
         except urllib2.URLError, e:
-            if e.code == 404: 
-                client.send(xmpp.Message(mess.getFrom(),"Sorry, but weather is not available. Probably you input incorrect CODE or its weather not cached."))
+            if e.code == 404:
+                conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. Probably you input incorrect CODE or its weather not cached."))
             else:
-                client.send(xmpp.Message(mess.getFrom(),"Sorry, but weather is not available. METAR weather caching site seems down."))
+                conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. METAR weather caching site seems down."))
         else:
             # 200, 301
             if ( urllib2.urlopen(strURL).getcode() not in [200, 301] ):
-                client.send(xmpp.Message(mess.getFrom(),"Sorry, but weather is not available. METAR weather caching site seems down."))
+                conn.send(xmpp.Message(msg.getFrom(),"Sorry, but weather is not available. METAR weather caching site seems down."))
             else:
                 f = urllib2.urlopen(urllib2.Request(strURL))
                 response = f.read()
                 f.close()
-                client.send(xmpp.Message(mess.getFrom(),response))
+                conn.send(xmpp.Message(msg.getFrom(),response))
     else:
-        client.send(xmpp.Message(mess.getFrom(),"Usage: CODE\nYou can find out code of your city airport here - http://www.rap.ucar.edu/weather/surface/stations.txt"))
+        conn.send(xmpp.Message(msg.getFrom(),"Usage: CODE\nYou can find out code of your city airport here - http://www.rap.ucar.edu/weather/surface/stations.txt"))
 
-client.RegisterHandler('message',message)
-client.RegisterDisconnectHandler(client.reconnectAndReauth())
-client.sendInitPresence()
-client.RegisterHandler('presence', presence)
 
-while True:
-    client.Process(1)
+def presenceCB(conn,msg):
+    if ( msg.getType() == "subscribe" ):
+        conn.send(xmpp.Presence(to=msg.getFrom(), typ='subscribed'))
+    
+def StepOn(conn):
+    try:
+        conn.Process(1)
+    except KeyboardInterrupt:
+        return 0
+    return 1
+    
+def GoOn(conn):
+    while StepOn(conn): pass
+    conn.disconnect()
+        
+def main():
+    config = ConfigParser.ConfigParser()
+    config.read('/etc/xmppwz.conf')
+    user=(config.get('account', 'login'))
+    password=(config.get('account', 'password'))
+    presence=(config.get('presence','presence'))
+    jid=xmpp.protocol.JID(user)
+    cl = xmpp.Client(jid.getDomain(), debug=[])
+    if cl.connect() == "":
+        print "not connected"
+        sys.exit(0)
+    if cl.auth(jid.getNode(),password,"METAR weather bot") == None:
+        print "authentication failed"
+        sys.exit(0)
+    cl.UnregisterDisconnectHandler(cl.DisconnectHandler)
+    cl.RegisterDisconnectHandler(cl.reconnectAndReauth())
+    cl.RegisterHandler('message', messageCB)
+    cl.RegisterHandler('presence', presenceCB)
+    cl.sendInitPresence()
+    GoOn(cl)
+    
+main()
